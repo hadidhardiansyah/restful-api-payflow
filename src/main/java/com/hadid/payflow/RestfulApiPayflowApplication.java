b6dee64e63dfdc27e6e5a3b1000513bae0ba7ba4
Hadid Hardiansyah <hadidhardiansyah@gmail.com>
2025-04-23 12:05:48 +0700

    Initial commit

---
+++ src/main/java/com/hadid/payflow/RestfulApiPayflowApplication.java
@@ -0,0 +1,13 @@
+package com.hadid.payflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulApiPayflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulApiPayflowApplication.class, args);
+	}
+
+}
